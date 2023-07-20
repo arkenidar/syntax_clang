0c3d4ff6036c9a1b28c57b7f5f815eb2eeaa6d86
Dario Cangialosi <dario.cangialosi@gmail.com>
2023-07-21 00:23:59 +0200

    // https://en.wikibooks.org/wiki/C_Programming/stdbool.h

--- bool-enum.c
+++ bool-enum.c
@@ -1,6 +1,9 @@
 
 #include <stdio.h>
 
+// https://en.wikibooks.org/wiki/C_Programming/stdbool.h
+#include <stdbool.h>
+
 enum boolean_enum {
 boolean_false=0, boolean_true=1
 };
@@ -8,4 +11,8 @@ boolean_false=0, boolean_true=1
 int main(){
     enum boolean_enum condition = boolean_false;
     printf("OUT: %s %d\n", condition!=0 ? "True!" : "False!" , (int)condition );
+
+    puts("#include <stdbool.h>");
+    bool boolean_variable = true;
+    puts(boolean_variable ? "True!" : "False!" );
 }
