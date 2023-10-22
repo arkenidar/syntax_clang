570fc36c1b67428073f312d1a1037487f1bf8991
Dario Cangialosi <dario.cangialosi@gmail.com>
2023-10-22 23:19:17 +0200

    [pointers.c] test 2 added for struct ComplexData

--- pointers.c
+++ pointers.c
@@ -5,11 +5,54 @@ struct Matrix{
 	int * p;
 };
 
-int main(){
+void test1(){
+	
+	puts("--------------------");
+	puts("TEST 1");
+	puts("--------------------");
+
 	struct Matrix matrix;
 	
 	matrix.n=111;
 	
 	int got=*(((int*)&matrix)+1);
 	printf("%d\n",got);
+
+	puts("--------------------");
+}
+
+struct ComplexData
+{
+	int steps;
+	float distance;
+	char* name;
+};
+
+void test2(){
+	
+	puts("--------------------");
+	puts("TEST 2");
+	puts("--------------------");
+
+	struct ComplexData structure;
+
+	structure.steps = 100;
+	structure.distance = 55.5;
+	structure.name = (char*)"NAME";
+
+	int* psteps = (int*)&structure;
+	float* pdistance = (float*)(psteps+1);
+	char** pname = (char**)(pdistance+1);
+
+	printf("int %d\n", *psteps );
+	printf("float %g\n", *pdistance );
+	printf("char* %s\n", *pname );
+
+	puts("--------------------");
+}
+
+int main(){
+	test1();
+	puts("===========================");
+	test2();
 }
